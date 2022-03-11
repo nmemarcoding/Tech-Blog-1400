9c42006ebca4e8b52f002e90340db0b4df6cf100
nmemarcoding <nmemarcoding@gmail.com>
2022-03-11 10:45:10 -0800

    url added

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
-    ? new Sequelize(process.env.JAWSDB_URL)
-    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+const sequelize = 'mysql://fof0k5khp8gxmc49:hjkmeb7ektp1vanv@m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vlyhkpn2pep4faxv' ?
+    new Sequelize('mysql://fof0k5khp8gxmc49:hjkmeb7ektp1vanv@m7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/vlyhkpn2pep4faxv') :
+    new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
